04b5a58616435b5a1de95ae745992b689dd09f78
ShivankTyagi <tyagishivank1212@gmail.com>
2023-03-01 13:36:07 +0530

    shivank

--- src/reducers/counterReducer.js
+++ src/reducers/counterReducer.js
@@ -1,5 +1,20 @@
-const counterReducer = (state,action) =>{
+const counterReducer = (state, action) => {
+    switch (action.type) {
+        case 'increment':
+            //code
+            return {
+                ...state, value: state.value + 1
+            }
 
+        case 'decrement':
+            //code
+            return {
+                ...state, value: state.value - 1
+            }
+
+        default:
+            return state;
+    }
 }
 
-export {counterReducer}
+export { counterReducer }
